bff189147f55251f984a3577008388f0eef55836
Ramirez-Moises <mrmoimoibee@gmail.com>
2022-04-06 19:15:06 -0700

    I logged my name and favorite food to the console.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// this is a single line comment
+/* this is supposed to be a multiple line comment 
+this is the second line of my multiple line comment 
+I can press enter to start the next line */ 
+
+console.log("Moises Emanuel Ramirez")
+
+console.log("My favorite food currently is Pho.")
