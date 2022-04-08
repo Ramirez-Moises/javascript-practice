e82b73de6d33426d186d89025a220044850c15bb
Ramirez-Moises <mrmoimoibee@gmail.com>
2022-04-07 18:28:42 -0700

    added a constant to my js that the log calls on

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@
 this is the second line of my multiple line comment 
 I can press enter to start the next line */ 
 
-console.log("Moises Emanuel Ramirez")
+const myName = {
+    firstname: "Moises" , lastname: "Ramirez" , favoritefood: "Pho"
+}
 
-console.log("My favorite food currently is Pho.")
+console.log(myName)
+
+// console.log("Moises Emanuel Ramirez")
+
+// console.log("My favorite food currently is Pho.")
